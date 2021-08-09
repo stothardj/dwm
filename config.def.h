4a496701adbc6f15f81de72146d765914a7c4ff8
Jake Stothard <stothardj@gmail.com>
2021-08-08 23:06:10 -0700

    Change back to Mod+r for commands

--- config.def.h
+++ config.def.h
@@ -88,8 +88,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_e,      tagrename,      {0} },
-	{ MODKEY|ShiftMask,             XK_e,      viewbyname,     {0} },
+	{ MODKEY,                       XK_r,      tagrename,      {0} },
+	{ MODKEY|ShiftMask,             XK_r,      viewbyname,     {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
